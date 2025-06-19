2c7de76c5b8043ee62864fb1cb652db0da660ef5
lastforever457 <lastforever457@gmail.com>
2025-06-19 14:39:15 +0500

    add sitemap

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -14,6 +14,11 @@ const eslintConfig = [
   {
     rules: {
       'react/no-unescaped-entities': 'off',
+      '@typescript-eslint/no-unused-vars': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-empty-function': 'off',
+      '@typescript-eslint/no-misused-promises': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
     },
   },
 ]
